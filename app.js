d2ec441f351b4169f9a8642a9bc6caaa9c2e4924
basit1215 <haizabdulbasit225@gmail.com>
2024-04-27 17:45:05 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
-var userInp= +prompt("Enter any positive integer");
-document.write("Number: " + userInp + "<br>" + " Round off value of the number: " + Math.round(userInp) + "<br>"+  "Floor value of the number: " + Math.floor(userInp)  + "<br>" + "Ceil value of the number: " + Math.ceil(userInp) + "<br>" + "<br>" );
+// var userInp= +prompt("Enter any positive integer");
+// document.write("Number: " + userInp + "<br>" + " Round off value of the number: " + Math.round(userInp) + "<br>"+  "Floor value of the number: " + Math.floor(userInp)  + "<br>" + "Ceil value of the number: " + Math.ceil(userInp) + "<br>" + "<br>" );
 
-var userInp2= +prompt("Enter any negative integer");
-document.write("Number: " + userInp2 + "<br>" + " Round off value of the number: " + Math.round(userInp2) + "<br>"+  "Floor value of the number: " + Math.floor(userInp2)  + "<br>" + "Ceil value of the number: " + Math.ceil(userInp2)  + "<br>" + "<br>" );
+// var userInp2= +prompt("Enter any negative integer");
+// document.write("Number: " + userInp2 + "<br>" + " Round off value of the number: " + Math.round(userInp2) + "<br>"+  "Floor value of the number: " + Math.floor(userInp2)  + "<br>" + "Ceil value of the number: " + Math.ceil(userInp2)  + "<br>" + "<br>" );
 
-var num= -5 ;
-document.write("The absolute value of " + num + " is " + Math.abs(num)+ "<br>"+ "<br>");
+// var num= -5 ;
+// document.write("The absolute value of " + num + " is " + Math.abs(num)+ "<br>"+ "<br>");
+
+// var dice1 = Math.floor(Math.random() * 6 + 1) ;
+// var dice2 = Math.floor(Math.random() * 6 + 1) ;
+// document.write("Random Dice 1 Value is: "+ dice1  + "<br>" +"Random Dice 2 Value is: "+ dice2  + "<br>"  + "<br>");
+
+// var coins= Math.floor(Math.random() *2 +1);
+// if(coins===1){
+// document.write(coins + "<br>"+ "Random coin value: Tails"+ "<br>"+ "<br>");
+// }
+// else if(coins===2){
+//     document.write(coins + "<br>"+ "<br>" + "Random coin value: Heads"+ "<br>"+ "<br>");
+// }
+// else{
+//     document.write("Error"+ "<br>"+ "<br>");
+// }
+
+// var randomNum= Math.floor(Math.random() *100 +1);
+// document.write("Random number between 1 and 100: " + randomNum+ "<br>"+ "<br>");
 
