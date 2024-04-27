028108b3190d22cf343d5e38be95abf50cc45d1d
basit1215 <haizabdulbasit225@gmail.com>
2024-04-27 20:11:08 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var userInp= +prompt("Enter any positive integer");
 document.write("Number: " + userInp + "<br>" + " Round off value of the number: " + Math.round(userInp) + "<br>"+  "Floor value of the number: " + Math.floor(userInp)  + "<br>" + "Ceil value of the number: " + Math.ceil(userInp) + "<br>" + "<br>" );
 
+var userInp2= +prompt("Enter any negative integer");
 document.write("Number: " + userInp2 + "<br>" + " Round off value of the number: " + Math.round(userInp2) + "<br>"+  "Floor value of the number: " + Math.floor(userInp2)  + "<br>" + "Ceil value of the number: " + Math.ceil(userInp2)  + "<br>" + "<br>" );
 
 var num= -5 ;
